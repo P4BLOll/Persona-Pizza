911448fb3435b2554b9ff9b96aa0346fcebbc79f
Pablo Santos <99838818+P4BLOll@users.noreply.github.com>
2023-08-29 11:00:59 -0300

    Add files via upload

---
+++ personapizza/carrinho.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Carrinho</title>
+</head>
+<body>
+    <h1>Seu Carrinho</h1>
+    <ul>
+        <?php
+        $total = 0;
+
+        $qtdProduto1 = $_POST['produto1'];
+        $qtdProduto2 = $_POST['produto2'];
+        $qtdProduto3 = $_POST['produto3'];
+
+        if ($qtdProduto1 > 0) {
+            $total += $qtdProduto1 * 10;
+            echo "<li>Produto 1 - $qtdProduto1 unidades</li>";
+        }
+        if ($qtdProduto2 > 0) {
+            $total += $qtdProduto2 * 20;
+            echo "<li>Produto 2 - $qtdProduto2 unidades</li>";
+        }
+        if ($qtdProduto3 > 0) {
+            $total += $qtdProduto3 * 30;
+            echo "<li>Produto 3 - $qtdProduto3 unidades</li>";
+        }
+
+        echo "<li>Total: $" . $total . "</li>";
+        ?>
+    </ul>
+</body>
+</html>
---
+++ personapizza/db.php
@@ -0,0 +1,20 @@
+<?php
+$host = 'seu_host';
+$db   = 'seu_banco_de_dados';
+$user = 'seu_usuario';
+$pass = 'sua_senha';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+?>
