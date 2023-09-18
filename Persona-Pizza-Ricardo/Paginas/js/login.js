0539dfc10af93052ab9fe48b5debb70d0314a885
Victor <126500655+VNav4rr0@users.noreply.github.com>
2023-09-18 10:22:00 -0300

    Delete Persona-Pizza-Ricardo directory

--- Persona-Pizza-Ricardo/Paginas/js/login.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const loginContainer = document.getElementById('login_container')
-
-const moveOverlay = () => loginContainer.classList.toggle('move')
-
-document.getElementById('abrir-cadastro').addEventListener('click', moveOverlay)
-document.getElementById('abrir-login').addEventListener('click', moveOverlay)
