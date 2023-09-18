b012296443fac30025e00c701124dcc978290dcc
Matheus <99839311+cooingcy@users.noreply.github.com>
2023-09-18 13:24:11 -0300

    Página de personalização
    Falta arrumar umas coisas.

---
+++ Paginas/Personalizacao_Pizza/index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accordion</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="accordion_item">
+                <hr>
+                <div class="accordion_header">
+                    <div class="bullet">1</div>
+                    <div class="title" style="margin-right: 500px;">Massa</div>
+                    <div class="icon">
+                        +
+                    </div>
+                </div>
+                <hr>
+                <div style="background-color: #121212">
+                    <div class="accordion_content">
+                    <div class="vertical-line"></div>
+                        <div class="panel" id="accordion1">
+                            <label><input type="radio" name="massa"></label>
+                            <label><input type="radio" name="massa"></label>
+                            <label><input type="radio" name="massa"></label><br>
+                            <label><input type="radio" name="massa"></label>
+                            <label><input type="radio" name="massa"></label>
+                            <label><input type="radio" name="massa"></label><br>
+                            <label><input type="radio" name="massa"></label>
+                            <label><input type="radio" name="massa"></label>
+                            <label><input type="radio" name="massa"></label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion_item">
+                <hr>
+                <div class="accordion_header">
+                    <div class="bullet">2</div>
+                    <div class="title" style="margin-right: 500px;">Molho</div>
+                    <div class="icon">
+                        +
+                    </div>
+                </div>
+                <hr>
+                <div style="background-color: #121212">
+                    <div class="accordion_content">
+                    <div class="vertical-line"></div>
+                        <div class="panel" id="accordion2">
+                            <label><input type="radio" name="molho"></label>
+                            <label><input type="radio" name="molho"></label>
+                            <label><input type="radio" name="molho"></label><br>
+                            <label><input type="radio" name="molho"></label>
+                            <label><input type="radio" name="molho"></label>
+                            <label><input type="radio" name="molho"></label><br>
+                            <label><input type="radio" name="molho"></label>
+                            <label><input type="radio" name="molho"></label>
+                            <label><input type="radio" name="molho"></label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion_item">
+                <hr>
+                <div class="accordion_header">
+                    <div class="bullet">3</div>
+                    <div class="title" style="margin-right: 485px;">Recheio</div>
+                    <div class="icon">
+                        +
+                    </div>
+                </div>
+                <hr>
+                <div style="background-color: #121212">
+                    <div class="accordion_content">
+                    <div class="vertical-line"></div>
+                        <div class="panel" id="accordion3">
+                            <label><input type="radio" name="recheio"></label>
+                            <label><input type="radio" name="recheio"></label>
+                            <label><input type="radio" name="recheio"></label><br>
+                            <label><input type="radio" name="recheio"></label>
+                            <label><input type="radio" name="recheio"></label>
+                            <label><input type="radio" name="recheio"></label><br>
+                            <label><input type="radio" name="recheio"></label>
+                            <label><input type="radio" name="recheio"></label>
+                            <label><input type="radio" name="recheio"></label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion_item">
+                <hr>
+                <div class="accordion_header">
+                    <div class="bullet">4</div>
+                    <div class="title" style="margin-right: 500px;">Borda</div>
+                    <div class="icon">
+                        +
+                    </div>
+                </div>
+                <hr>
+                <div style="background-color: #121212">
+                    <div class="accordion_content">
+                    <div class="vertical-line"></div>
+                        <div class="panel" id="accordion4">
+                            <label><input type="radio" name="borda"></label>
+                            <label><input type="radio" name="borda"></label>
+                            <label><input type="radio" name="borda"></label><br>
+                            <label><input type="radio" name="borda"></label>
+                            <label><input type="radio" name="borda"></label>
+                            <label><input type="radio" name="borda"></label><br>
+                            <label><input type="radio" name="borda"></label>
+                            <label><input type="radio" name="borda"></label>
+                            <label><input type="radio" name="borda"></label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion_item">
+                <hr>
+                <div class="accordion_header">
+                    <div class="bullet">5</div>
+                    <div class="title" style="margin-right: 400px;">Acompanhamento</div>
+                    <div class="icon">
+                        +
+                    </div>
+                </div>
+                <hr>
+                <div style="background-color: #121212;">
+                    <div class="accordion_content">
+                    <div class="vertical-line"></div>
+                        <div class="panel" id="accordion5">
+                            <label><input type="radio" name="acompanhamento"></label>
+                            <label><input type="radio" name="acompanhamento"></label>
+                            <label><input type="radio" name="acompanhamento"></label><br>
+                            <label><input type="radio" name="acompanhamento"></label>
+                            <label><input type="radio" name="acompanhamento"></label>
+                            <label><input type="radio" name="acompanhamento"></label><br>
+                            <label><input type="radio" name="acompanhamento"></label>
+                            <label><input type="radio" name="acompanhamento"></label>
+                            <label><input type="radio" name="acompanhamento"></label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button type="button" class="red-button">Realizar pedido</button>
+
+
+    <script src="script.js"></script>
+</body>
+
+</html>
