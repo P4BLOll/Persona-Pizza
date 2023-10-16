3239f7983de3e60db737190627df749fb2f8522a
Matheus <99839311+cooingcy@users.noreply.github.com>
2023-10-16 01:53:49 -0300

    Atualização da Página Personalizar

---
+++ Paginas/Personalizacao_Pizza/navbar.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../criarpizza/css/navbar.css">
+    <title>Document</title>
+</head>
+<body>
+<nav>
+      <div class="logo">
+        <i class="bx bx-menu menu-icon"></i>
+        <span class="logo-name">PersonaPizza</span>
+        <ul>
+          <li>
+            <a href="home.php">Home</a>
+            <a href="perfil.php">Conta</a>
+            <a href="cardapio.php">Cardápio</a>
+            <a href="social_media.php">Social</a>
+            <a href="">Carrinho</a>
+          </li>
+          <div>
+            <a href="index.php" class="nav-link">
+              <i class="bx bx-log-in icon"></i>
+              <span class="link">Entrar</span>
+            </a>
+          </div>
+        </ul>
+      </div>
+      <div class="sidebar">
+        <div class="logo">
+          <i class="bx bx-arrow-back menu-icon"></i>
+        </div>
+
+        <div class="sidebar-content">
+          <ul class="lists">
+            <li class="list">
+              <a href="home.php" class="nav-link">
+                <i class="bx bx-home-alt icon"></i>
+                <span class="link">Home</span>
+              </a>
+            </li>
+            <li class="list">
+              <a href="perfil.php" class="nav-link">
+                <i class="bx bx-user icon"></i>
+                <span class="link">Conta</span>
+              </a>
+            </li>
+            <li class="list">
+              <a href="cardapio.php" class="nav-link">
+                <i class="bx bx-category icon"></i>
+                <span class="link">Cardápio</span>
+              </a>
+            </li>
+            <li class="list">
+              <a href="#" class="nav-link">
+                <i class="bx bx-cart icon"></i>
+                <span class="link">Carrinho</span>
+              </a>
+            </li>
+            <li class="list">
+              <a href="social_media.php" class="nav-link">
+                <i class="bx bx-message-square-dots icon"></i>
+                <span class="link">Social</span>
+              </a>
+            </li>
+          </ul>
+
+          <div class="bottom-cotent">
+            <li class="list sair">
+              <a href="index.php" class="nav-link sair-a">
+                <i class="bx bx-log-in icon"></i>
+                <span class="link">Entrar</span>
+              </a>
+            </li>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <section class="overlay"></section>
+</body>
+</html>
