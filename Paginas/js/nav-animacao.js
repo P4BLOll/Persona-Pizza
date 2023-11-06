41d44786469c93176c67ca97656691fd0d951e90
FATEC ZONA LESTE <joaopedrosilvadeoliveira56@gmail.com>
2023-11-06 14:47:14 -0300

    Atualização páginas Produto e Cardapio

--- Paginas/js/nav-animacao.js
+++ Paginas/js/nav-animacao.js
@@ -7,3 +7,26 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("visible");
   }
 });  
+
+function checkScreenSize() {
+  if (window.innerWidth > 768) { 
+    window.removeEventListener("scroll", scrollHandler); 
+    const navbar = document.getElementById("navbar");
+    navbar.classList.add("visible"); 
+  }
+}
+function scrollHandler() {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("visible");
+  } else {
+    navbar.classList.remove("visible");
+  }
+}
+
+window.addEventListener("load", checkScreenSize);
+
+
+window.addEventListener("resize", checkScreenSize);
+
+checkScreenSize();
